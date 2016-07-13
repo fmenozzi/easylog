7e557d07a7010ba5083fe16b9dbc3094580bc5f6
Federico Menozzi <federicogmenozzi@gmail.com>
2016-07-12 23:31:07 -0400

    Allow logging to specific file

--- easylog.c
+++ easylog.c
@@ -2,22 +2,20 @@
 
 #include <stdarg.h>
 
+#define EASYLOG_LOG_VARARG(loglevel, file, fmt, args)   \
+    fprintf(file, "%s: ", loglevel_str(level));         \
+    vfprintf(file, fmt, args);                          \
+
 logstruct easylog_global_log;
 
 static const char* loglevel_str(loglevel level) {
     switch (level) {
-        case INFO:
-            return "INFO";
-        case DEBUG:
-            return "DEBUG";
-        case WARNING:
-            return "WARNING";
-        case ERROR:
-            return "ERROR";
-        case FATAL:
-            return "FATAL";
-        default:
-            return "<INVALID LOG LEVEL>";
+        case INFO:    return "INFO";
+        case DEBUG:   return "DEBUG";
+        case WARNING: return "WARNING";
+        case ERROR:   return "ERROR";
+        case FATAL:   return "FATAL";
+        default:      return "<INVALID LOG LEVEL>";
     }
 }
 
@@ -26,19 +24,17 @@ void easylog_init(FILE* file) {
 }
 
 void easylog_log_file(loglevel level, FILE* file, const char* fmt, ...) {
-    va_list argptr;
-    va_start(argptr, fmt);
-    fprintf(file, "%s: ", loglevel_str(level));
-    vfprintf(file, fmt, argptr);
-    va_end(argptr);
+    va_list args;
+    va_start(args, fmt);
+    EASYLOG_LOG_VARARG(level, file, fmt, args);
+    va_end(args);
 }
 
 void easylog_log(loglevel level, const char* fmt, ...) {
-    va_list argptr;
-    va_start(argptr, fmt);
-    fprintf(easylog_global_log.file, "%s: ", loglevel_str(level));
-    vfprintf(easylog_global_log.file, fmt, argptr);
-    va_end(argptr);
+    va_list args;
+    va_start(args, fmt);
+    EASYLOG_LOG_VARARG(level, easylog_global_log.file, fmt, args);
+    va_end(args);
 
 }
 
