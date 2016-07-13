bf2778bdcb8d0dc422cec1b57b97961c061f4ead
Federico Menozzi <federicogmenozzi@gmail.com>
2016-07-13 18:57:53 -0400

    Add comments

--- easylog.c
+++ easylog.c
@@ -17,12 +17,19 @@ static const char* loglevel_str(loglevel level) {
     }
 }
 
+/*
+ * Initializes global log file
+ */
 void easylog_init(FILE* file) {
     if (file) {
         easylog_global_log.file = file;
     }
 }
 
+/*
+ * Helper function for forwarding varargs to a file. Also takes care of
+ * appending a newline automatically to the format string.
+ */
 static void easylog_log_vararg(loglevel level, FILE* file, const char* fmt, va_list args) {
     char newfmt[EASYLOG_MAX_LINE_LEN];
 
@@ -30,6 +37,10 @@ static void easylog_log_vararg(loglevel level, FILE* file, const char* fmt, va_l
     vfprintf(file, newfmt, args);
 }
 
+/*
+ * Log to a specific file. Use easylog_config_logfile() to permanently
+ * change the global log file.
+ */
 void easylog_log_file(loglevel level, FILE* file, const char* fmt, ...) {
     if (file && fmt) {
         va_list args;
@@ -39,6 +50,9 @@ void easylog_log_file(loglevel level, FILE* file, const char* fmt, ...) {
     }
 }
 
+/*
+ * Log to the current log file
+ */
 void easylog_log(loglevel level, const char* fmt, ...) {
     if (fmt) {
         va_list args;
@@ -48,6 +62,11 @@ void easylog_log(loglevel level, const char* fmt, ...) {
     }
 }
 
+/*
+ * Closes current log file and re-assigns it to file. The user
+ * should NOT attempt to close the file passed here, as the
+ * function effectively takes ownership of the file.
+ */
 void easylog_config_logfile(FILE* file) {
     if (file) {
         if (easylog_global_log.file) {
@@ -57,6 +76,9 @@ void easylog_config_logfile(FILE* file) {
     }
 }
 
+/*
+ * Close current log file
+ */
 void easylog_destroy() {
     if (easylog_global_log.file) {
         fclose(easylog_global_log.file);
